f4bfc285bafd44cd7a76bed1dd2cf76b74372269
Hospice HOUNSOU <hospice.h@makeba.money>
2021-01-22 19:55:30 +0000

    Number line jumps

---
+++ src/NumberLineJumps.kt
@@ -0,0 +1,46 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the kangaroo function below.
+fun kangaroo(positionKan1: Int, jumpDistanceKan1: Int, positionKan2: Int, jumpDistanceKan2: Int): String {
+    return if (jumpDistanceKan1 > jumpDistanceKan2) {
+        if ((positionKan1 -positionKan2)%(jumpDistanceKan2-jumpDistanceKan1)==0) {
+            "YES"
+        } else "NO"
+    } else{
+        "NO"
+    }
+}
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val x1V1X2V2 = scan.nextLine().split(" ")
+
+    val x1 = x1V1X2V2[0].trim().toInt()
+
+    val v1 = x1V1X2V2[1].trim().toInt()
+
+    val x2 = x1V1X2V2[2].trim().toInt()
+
+    val v2 = x1V1X2V2[3].trim().toInt()
+
+    val result = kangaroo(x1, v1, x2, v2)
+
+    println(result)
+}
